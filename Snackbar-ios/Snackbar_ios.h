07c6ccdb6d3ae2bec7999adaa85ee34ecad3e2e4
takao <hrgc.tko0717@gmail.com>
2018-04-29 17:01:56 +0900

    create project

---
+++ Snackbar-ios/Snackbar_ios.h
@@ -0,0 +1,19 @@
+//
+//  Snackbar_ios.h
+//  Snackbar-ios
+//
+//  Created by Takao Horiguchi on 2018/04/29.
+//  Copyright © 2018年 kakaocom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Snackbar_ios.
+FOUNDATION_EXPORT double Snackbar_iosVersionNumber;
+
+//! Project version string for Snackbar_ios.
+FOUNDATION_EXPORT const unsigned char Snackbar_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Snackbar_ios/PublicHeader.h>
+
+
